04fee060034e6f6b13df45f392b05bbd2c4fd3f3
Sahil Lather <sahillather002@gmail.com>
2025-10-17 18:57:36 +0000

    cleaning extras

--- components/commons/titlePrimary.tsx
+++
@@ -1,9 +0,0 @@
-type props = {
-  title: string;
-};
-
-const TitlePrimary = ({ title }: props) => {
-  return <h3 className="font-semibold text-[28px] text-primary">{title}</h3>;
-};
-
-export default TitlePrimary;
